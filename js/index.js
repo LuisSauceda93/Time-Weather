0d3904173bf8a344c4648c5c7b461458a2320765
LuisSauceda93 <luigalberto93@hotmail.com>
2015-11-14 15:57:06 -0600

    Se modifico el archivo index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,17 @@
 (
   function () {
+var API_WEATHER_KEY= "a5dbaad62fe7199046e6c6dbf005c544";
+var API_WEATHER_URL= "http://api.openweathermap.org/data/2.5/weather?APPID=" + API_WEATHER_KEY + "&";
+
+var cityWeather = {};
+cityWeather.zone;
+cityWeather.icon;
+cityWeather.temp;
+cityWeather.temp_max;
+cityWeather.temp_min;
+cityWeather.main;
+
+
     if(navigator.geolocation){
       navigator.geolocation.getCurrentPosition(getCoords, errorFound);
     }else {
@@ -26,10 +38,15 @@
         //3: Timeout
       };
 
-      function getCoords(position) {
+      function getCoords (position) {
         var lat= position.coords.latitude;
         var lon= position.coords.longitude;
-        sweetAlert("Tu posición es:\n"+lat+ " "+lon);
-        // body...
+        sweetAlert("Tu posición es:\n"+lat+ ", "+lon);
+        $.getJSON(API_WEATHER_URL + "lat= "+ lat + "&lon=" +lon, getCurrentWeather);
+
+        function getCurrentWeather(data) {
+          console.log(data);
+
+        }
       }
 })();
